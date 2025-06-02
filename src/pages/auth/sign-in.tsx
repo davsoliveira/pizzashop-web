40a8f962f0a047dd6bf669f1ab41f28bfd76e85b
Davi Oliveira <davi.ol.santos@gmail.com>
2025-06-02 09:31:37 -0300

    criando página de cadastro do restaurante

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -3,6 +3,7 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
@@ -21,7 +22,7 @@ export function SignIn() {
 
     await new Promise((resolve) => setTimeout(resolve, 2000))
 
-    toast.success('Enviamos um link de autenticação para sue e-mail')
+    toast.success('Enviamos um link de autenticação para seu e-mail')
   }
 
   return (
@@ -29,6 +30,8 @@ export function SignIn() {
       <Helmet title="Login" />
 
       <div className="p-8">
+        <Button variant={'ghost'} asChild className='absolute right-8 top-8'><Link to={'/sign-up'}>Novo estabelecimento</Link></Button>
+
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
