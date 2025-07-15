f89be5f92bc687ca614306097c9218a30fa90bb8
Davi Oliveira <davi.ol.santos@gmail.com>
2025-07-14 21:11:18 -0300

    criando a listagem de pedidos

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -3,8 +3,15 @@ import { Table, TableBody, TableHead, TableHeader, TableRow } from '@/components
 import { OrderTableRow } from './order-table-row'
 import { OrderTableFilters } from './order-table-filters'
 import { Pagination } from '@/components/pagination'
+import { useQuery } from '@tanstack/react-query'
+import { getOrders } from '@/api/get-orders'
 
 export function Orders() {
+  const { data: result } = useQuery({
+    queryKey: ['orders'],
+    queryFn: getOrders,
+  })
+
   return (
     <>
       <Helmet title="Pedidos" />
@@ -29,9 +36,10 @@ export function Orders() {
                 </TableRow>
               </TableHeader>
               <TableBody>
-                {Array.from({ length: 10 }).map((_, i) => (
-                  <OrderTableRow key={i} />
-                ))}
+                {result &&
+                  result.orders.map((order) => {
+                    return <OrderTableRow key={order.orderId} order={order} />
+                  })}
               </TableBody>
             </Table>
           </div>
