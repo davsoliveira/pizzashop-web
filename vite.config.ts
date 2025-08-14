4b8c63ba45742575f02390af0017ec8fc35a4f2c
Davi Oliveira <davi.ol.santos@gmail.com>
2025-08-14 09:11:40 -0300

    build: setup do ambiente de testes

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import type { UserConfig } from 'vite'
+import type { InlineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +11,11 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  test: {
+    globals: true,
+    setupFiles: ['./test/setup.ts'],
+    environment: 'happy-dom',
+  },
+} as UserConfig & {
+  test: InlineConfig
 })
