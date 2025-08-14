22323f4c02efebbdc2ce27b7b5b86c2806ebb6b5
Davi Oliveira <davi.ol.santos@gmail.com>
2025-08-14 08:38:20 -0300

    feat: adicionando página de Error

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,12 +7,13 @@ import { SignUp } from './pages/auth/sing-up'
 import { Orders } from './pages/app/orders/orders'
 import { Dashboard } from './pages/app/dashboard/dashboard'
 import { NotFound } from './pages/404'
+import { Error } from './pages/error'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
-    errorElement: <NotFound />,
+    errorElement: <Error />,
     children: [{ path: '/', element: <Dashboard /> },
     { path: '/orders', element: <Orders /> }
     ],
@@ -22,4 +23,8 @@ export const router = createBrowserRouter([
     element: <AuthLayout />,
     children: [{ path: '/sign-in', element: <SignIn /> }, { path: '/sign-up', element: <SignUp /> }],
   },
+  {
+    path: '*',
+    element: <NotFound />
+  }
 ])
